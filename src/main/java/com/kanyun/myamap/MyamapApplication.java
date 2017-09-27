672d4cb50686189d91b110795c16cef96e9d06cf
zhaoyingxu <949955482@qq.com>
2017-09-27 18:28:28 +0800

    ddd

---
+++ src/main/java/com/kanyun/myamap/MyamapApplication.java
@@ -0,0 +1,12 @@
+package com.kanyun.myamap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyamapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyamapApplication.class, args);
+	}
+}
